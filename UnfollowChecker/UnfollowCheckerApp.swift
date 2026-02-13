f55d56bd8f144d3c8406a139f4b4718f6e14750f
grizzolo <rizzolo.giovanni98@gmail.com>
2026-02-13 23:31:00 +0100

    Initial Commit

---
+++ UnfollowChecker/UnfollowCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnfollowCheckerApp.swift
+//  UnfollowChecker
+//
+//  Created by Giovanni Rizzolo on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct UnfollowCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
